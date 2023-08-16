4a2b9672c59f8db92c70b236780c6efadb33b3f7
Edwin Cubillos <17167246+Cubillosxy@users.noreply.github.com>
2023-08-16 11:30:57 -0500

    Fix admin site

--- bookings/admin.py
+++ bookings/admin.py
@@ -14,8 +14,8 @@ class BookingAdmin(admin.ModelAdmin):
         "updated_at",
     )
     list_filter = ("is_cancelled", "is_active", "created_at", "updated_at")
-    search_fields = "user__email"
-    ordering = ("-created_at",)  # Newest bookings come first
+    search_fields = ["user__email"]
+    ordering = ("-created_at",)
     date_hierarchy = "created_at"
     readonly_fields = ("created_at", "updated_at")
 
